baf6b17e7223e1a6d09d77096459ecdd9bfc5a9c
Gavin Carothers <gavin@carothers.name>
2011-12-31 09:40:20 -0800

    Changed stuff

--- js/script.js
+++ js/script.js
@@ -3,6 +3,44 @@
 */
 
 
+            Stripe.setPublishableKey('pk_5fvSxbLNlYHMDSU29G5fub7i6jEjF');
+
+            function stripeResponseHandler(status, response) {
+                if (response.error) {
+                    // re-enable the submit button
+                    $('.submit-button').removeAttr("disabled");
+                    // show the errors on the form
+                    $(".payment-errors").html(response.error.message);
+                } else {
+                    var form$ = $("#payment-form");
+                    // token contains id, last4, and card type
+                    var token = response['id'];
+                    // insert the token into the form so it gets submitted to the server
+                    form$.append("<input type='hidden' name='stripeToken' value='" + token + "' />");
+                    // and submit
+                    form$.get(0).submit();
+                }
+            }
+
+            $(document).ready(function() {
+                $("#payment-form").submit(function(event) {
+                    // disable the submit button to prevent repeated clicks
+                    $('.submit-button').attr("disabled", "disabled");
+                    var chargeAmount = 1000; //amount you want to charge, in cents. 1000 = $10.00, 2000 = $20.00 ...
+                    // createToken returns immediately - the supplied callback submits the form if there are no errors
+                    Stripe.createToken({
+                        number: $('.card-number').val(),
+                        cvc: $('.card-cvc').val(),
+                        exp_month: $('.card-expiry-month').val(),
+                        exp_year: $('.card-expiry-year').val()
+                    }, chargeAmount, stripeResponseHandler);
+                    return false; // submit from callback
+                });
+            });
+
+            if (window.location.protocol === 'file:') {
+                alert("stripe.js does not work when included in pages served over file:// URLs. Try serving this page over a webserver. Contact support@stripe.com if you need assistance.");
+            }
 
 
 
